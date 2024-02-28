148390bdf916d7ed70aa2e1acdb7865f637c6a1c
amberlynn <amberlynn364@gmail.com>
2024-02-28 19:14:51 +0300

    feat: implement GymAd component

---
+++ src/components/GymAd/GymAd.jsx
@@ -0,0 +1,12 @@
+import './GymAd.css';
+
+const GymAd = () => {
+  return (
+    <div className='gym-ad'>
+      <div></div>
+      <span>the best fitness experience</span>
+    </div>
+  )
+}
+
+export default GymAd
