9cbe2df928b941eecc37100960d65d0d8924f452
amberlynn <amberlynn364@gmail.com>
2024-02-28 19:15:34 +0300

    feat: implement HeaderMenu component

---
+++ src/components/Header/HeaderMenu/HeaderMenu.jsx
@@ -0,0 +1,15 @@
+import './HeaderMenu.css';
+
+const HeaderMenu = () => {
+  return (
+    <ul className='header-menu'>
+      <li>Home</li>
+      <li>Programs</li>
+      <li>Why us</li>
+      <li>Plans</li>
+      <li>Testimonials</li>
+    </ul>
+  )
+}
+
+export default HeaderMenu
