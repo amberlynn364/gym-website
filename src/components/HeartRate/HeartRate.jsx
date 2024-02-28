f34535be94308e0b22df2ee264aeba7e8f10675c
amberlynn <amberlynn364@gmail.com>
2024-02-28 19:15:53 +0300

    feat: implement HeartRate component

---
+++ src/components/HeartRate/HeartRate.jsx
@@ -0,0 +1,15 @@
+import './HeartRate.css';
+import Heart from '../../assets/heart.png';
+
+
+const HeartRate = () => {
+  return (
+    <div className='heart-rate'>
+      <img src={Heart} alt="" />
+      <span>Heart rate</span>
+      <span>116 bpm</span>
+    </div>
+  )
+}
+
+export default HeartRate
