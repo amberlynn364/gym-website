30f7be6c6b29d074441276bfea23330545081f4a
amberlynn <amberlynn364@gmail.com>
2024-02-28 19:15:24 +0300

    feat: implement HeaderLogo component

---
+++ src/components/Header/HeaderLogo/HeaderLogo.jsx
@@ -0,0 +1,8 @@
+import Logo from '../../../assets/logo.png';
+import './HeaderLogo.css'
+
+const HeaderLogo = () => {
+  return <img src={Logo} alt='' className='logo'></img>
+}
+
+export default HeaderLogo;
