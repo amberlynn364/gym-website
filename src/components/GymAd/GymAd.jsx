99589e2abf89811011a1d01d448320ec8ba0aab0
amberlynn <amberlynn364@gmail.com>
2024-03-01 15:49:17 +0300

    feat: add responsive for hero section

--- src/components/GymAd/GymAd.jsx
+++ src/components/GymAd/GymAd.jsx
@@ -3,11 +3,11 @@ import './GymAd.css';
 import { motion } from 'framer-motion';
 
 const GymAd = () => {
-
+  const mobileTransition = window.innerWidth <= 768 ? true : false;
   return (
     <div className='gym-ad'>
       <motion.div
-        initial={{ left: '180px' }}
+        initial={{ left: mobileTransition ? '135px' : '180px' }}
         whileInView={{ left: '8px' }}
         transition={{ ...TRANSITION, type: 'tween' }}
       />
